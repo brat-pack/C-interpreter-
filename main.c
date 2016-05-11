fd5b4af26e8a541cd12a3fe2d9205ef533c7137c
ThomOverhand <thom_overhand@hotmail.com>
2016-05-11 12:42:39 +0200

    Added file reading

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 // Created by Moro on 11/05/16.
 //
 
+#include <stdio.h>
+
+FILE *fr; /* declare file pointer */
+
 int main(int argc, char* argv[]) {
+
+    int c;
+
+    fr = fopen("LICENCE.txt", "rt");
+
+    while((c = getc(fr)) != EOF)
+        putchar(c);
+    fclose(fr);
     return 0;
 }
